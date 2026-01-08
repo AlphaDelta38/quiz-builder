37503b097bff62ac37eab6f2ed354afa87870e29
alphadelta38 <kiril.morozov.01@gmail.com>
2026-01-08 21:11:41 +0200

    add initial structure

---
+++ front-end/app/(navigation)/layout.tsx
@@ -0,0 +1,10 @@
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <h1>Layout</h1>
+      {children}
+    </div>
+  );
+}
+
+export default Layout;
---
+++ front-end/app/(navigation)/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./index.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}></h1>
+    </div>
+  );
+}
